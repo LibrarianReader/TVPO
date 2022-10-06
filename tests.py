b59c7eed563c893a12de94627efd7a1bf1198e05
Anton Moskovsky <79806705+rosewhat@users.noreply.github.com>
2022-10-06 12:14:29 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,19 +1,3 @@
 from unittest import TestCase, main
 from TestTask import minsort, maxsort
 
-class MinsortTest(TestCase):
-    def test_minT(self):
-        self.assertEqual(minsort(["Gon","Li"],"Li"),True)
-
-    def test_minF(self):
-        self.assertEqual(minsort(["Gon","Li"],"Gon"),False)
-
-class MaxsortTest(TestCase):
-    def test_maxF(self):
-        self.assertEqual(maxsort(["Gon","Li"],"Li"),False)
-
-    def test_maxT(self):
-        self.assertEqual(maxsort(["Gon","Li"],"Gon"),True)
-
-if __name__ == '__main__':
-    main()
